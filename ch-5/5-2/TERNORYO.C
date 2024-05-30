22b51cf17305ebe9e204d7acfedb9f9adf75da94
jenishbabariya22 <mr.immortalom09@gmail.com>
2024-05-30 18:08:30 +0530

    30/5/2024

---
+++ ch-5/5-2/TERNORYO.C
@@ -0,0 +1,33 @@
+#include<stdio.h>
+#include<conio.h>
+#define pf printf
+#define sf scanf
+
+
+main()
+{
+	    int a, b, c;
+	   clrscr();
+
+	   pf("enter the value of first number :");
+	   sf("%d",&a);
+
+	   pf("enter the value of second number :");
+	   sf("%d",&b);
+
+	   pf("enter the value of third number :");
+	   sf("%d",&c);
+
+	   pf("\n");
+
+
+
+
+
+
+
+
+
+	    getch();
+
+}
