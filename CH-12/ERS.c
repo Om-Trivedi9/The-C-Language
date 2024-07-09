8a2e7e9609818f48e68cf4ba28fe73da73bdf36b
unknown <mr.immortalom09@gmail.com>
2024-07-09 16:43:19 +0530

    9/7/2024

--- CH-12/ERS.c
+++ CH-12/ERS.c
@@ -4,13 +4,13 @@
 
 struct student
 {
-    int id[700];
-    int age[700];
-    char name[700];
-    char roal[700];
-    char city[700];
-    char expirianc[700];
-    char company_name[700];
+    int id;
+    int age;
+    char name[70];
+    char roal[70];
+    char city[70];
+    char expirianc[70];
+    char company_name[70];
 };
 
 void main()
@@ -26,23 +26,23 @@ void main()
     {
         system("cls");
         printf("enter your id :");
-        scanf("%d\n", &s1[i].id);
+        scanf("%d", &s1[i].id);
         printf("enter your age :");
-        scanf("%d\n", &s1[i].age);
+        scanf("%d", &s1[i].age);
         fflush(stdin);
         printf("enter your name :");
-        scanf("%S\n", &s1[i].name);
+        gets(s1[i].name);
         fflush(stdin);
         printf("enter your roal for company :");
         gets(s1[i].roal);
         fflush(stdin);
         printf("enter your expirianc :");
-        scanf("%S\n", &s1[i].expirianc);
+        gets(s1[i].expirianc);
         fflush(stdin);
         printf("enter your city :");
-        scanf("%S\n", &s1[i].city);
+        gets(s1[i].city);
         printf("enter your company name :");
-        scanf("%d\n", &s1[i].company_name);
+        gets(s1[i].company_name);
     }
 
     system("cls");
@@ -53,13 +53,13 @@ void main()
     {
         printf("employ delails output\n\n");
 
-        printf("styand ID : %d\n", s1[i].id);
-        printf("student Age : %d\n", s1[i].age);
-        printf("student name : %s\n", s1[i].name);
-        printf("student roal : %s\n", s1[i].roal);
-        printf("student city : %s\n", s1[i].city);
-        printf("student expirianc : %s\n", s1[i].expirianc);
-        printf("student company name : %s\n", s1[i].company_name);
+        printf("employ ID : %d\n", s1[i].id);
+        printf("employ Age : %d\n", s1[i].age);
+        printf("employ name : %s\n", s1[i].name);
+        printf("employ roal : %s\n", s1[i].roal);
+        printf("employ city : %s\n", s1[i].city);
+        printf("employ expirianc : %s\n", s1[i].expirianc);
+        printf("employ company name : %s\n", s1[i].company_name);
 
         printf("\n\n\n\n");
     }
